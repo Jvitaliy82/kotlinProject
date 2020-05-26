604a9cab56d73dde85210369f4f5f5205b626187
Jvitaliy82 <jvit82@gmail.com>
2020-05-26 11:40:59 +0700

    less8

--- app/src/test/java/com/jdeveloperapps/kotlinproject/data/provider/FireStoreProviderTest.kt
+++ app/src/test/java/com/jdeveloperapps/kotlinproject/data/provider/FireStoreProviderTest.kt
@@ -1,26 +1,32 @@
 package com.jdeveloperapps.kotlinproject.data.provider
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.jdeveloperapps.kotlinproject.data.entity.Note
+import com.jdeveloperapps.kotlinproject.data.errors.NoAuthExeption
+import com.jdeveloperapps.kotlinproject.data.model.NoteResult
 import com.jdeveloperapps.kotlinproject.data.providers.FireStoreProvider
-import io.mockk.clearAllMocks
-import io.mockk.every
-import io.mockk.mockk
+import io.mockk.*
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class FireStoreProviderTest {
 
     @get:Rule
     val taskExecutorRule = InstantTaskExecutorRule()
 
-    private val mockDb = mockk<FirebaseFirestore>()
     private val mockAuth = mockk<FirebaseAuth>()
+    private val mockDb = mockk<FirebaseFirestore>()
 
     private val mockResultCollection = mockk<CollectionReference>()
     private val mockUser = mockk<FirebaseUser>()
@@ -36,33 +42,23 @@ class FireStoreProviderTest {
     @Before
     fun setup() {
         clearAllMocks()
-        every { mockAuth.currentUser } returns mockUser
-        every { mockUser.uid} returns ""
-        every { mockDb.collection(any()).document(any()).collection(any()) } returns mockResultCollection
+        coEvery { mockAuth.currentUser } returns mockUser
+        coEvery { mockUser.uid } returns ""
+        coEvery { mockDb.collection(any()).document(any()).collection(any()) } returns mockResultCollection
 
-        every { mockDocument1.toObject(Note::class.java) } returns testNotes[0]
-        every { mockDocument2.toObject(Note::class.java) } returns testNotes[1]
-        every { mockDocument3.toObject(Note::class.java) } returns testNotes[2]
+        coEvery { mockDocument1.toObject(Note::class.java) } returns testNotes[0]
+        coEvery { mockDocument2.toObject(Note::class.java) } returns testNotes[1]
+        coEvery { mockDocument3.toObject(Note::class.java) } returns testNotes[2]
     }
 
-//    @Test
-//    fun `should throw NoAuthException if no auth`() {
-//        every { mockAuth.currentUser } returns null
-//        var result: Any? = null
-//        provider.subscribeToAllNotes().observeForever {
-//            result = (it as? NoteResult.Error)?.error
-//        }
-//        assertTrue(result is NoAuthExeption)
-//    }
-//
-//    @Test
-//    fun `save note calls set`() {
-//        val mockDocumentReference = mockk<DocumentReference>()
-//        every { mockResultCollection.document(testNotes[0].id) } returns mockDocumentReference
-//        provider.saveNote(testNotes[0])
-//        verify (exactly = 1){ mockDocumentReference.set(testNotes[0]) }
-//    }
-//
+    @Test
+    fun `should throw Error if no auth`() = runBlocking {
+        coEvery { mockAuth.currentUser } returns null
+        var result: Any? = null
+        result = provider.subscribeToAllNotes().receive()
+        assertTrue(result is NoteResult.Error)
+    }
+
 //    @Test
 //    fun `subscribeToAllNotes returns notes`() {
 //        var result: List<Note>? = null
@@ -97,37 +93,20 @@ class FireStoreProviderTest {
 //        assertEquals(result, mockExeption)
 //    }
 //
-//    @Test
-//    fun `saveNote returns Note`() {
-//        var result: Note? = null
-//        val mockDocumentReference = mockk<DocumentReference>()
-//        val slot = slot<OnSuccessListener<in Void>>()
-//
-//        every { mockResultCollection.document(testNotes[0].id) } returns mockDocumentReference
-//        every { mockDocumentReference.set(testNotes[0]).addOnSuccessListener(capture(slot)) } returns mockk()
-//
-//        provider.saveNote(testNotes[0]).observeForever {
-//            result = (it as? NoteResult.Success<Note>)?.data
-//        }
-//
-//        slot.captured.onSuccess(null)
-//        assertEquals(testNotes[0], result)
-//    }
-//
-//    @Test
-//    fun `deleteNote returns Note`() {
-//        var result: NoteResult? = null
-//        val mockDocumentReference = mockk<DocumentReference>()
-//        val slot = slot<OnSuccessListener<in Void>>()
-//
-//        every { mockResultCollection.document(testNotes[0].id) } returns mockDocumentReference
-//        every { mockDocumentReference.delete().addOnSuccessListener(capture(slot)) } returns mockk()
-//
-//        provider.deleteNote(testNotes[0].id).observeForever {
-//            result = it
-//        }
-//
-//        slot.captured.onSuccess(null)
-//        assertTrue(result is NoteResult.Success<*>)
-//    }
+    @Test
+    fun `saveNote returns Note`() = runBlocking{
+        var result: Note? = null
+        val mockDocumentReference = mockk<DocumentReference>()
+        val slot = slot<OnSuccessListener<in Void>>()
+
+        coEvery { mockResultCollection.document(testNotes[0].id) } returns mockDocumentReference
+        coEvery { mockDocumentReference.set(testNotes[0]).addOnSuccessListener(capture(slot)) } returns mockk()
+
+        result = provider.saveNote(testNotes[0])
+
+        slot.captured.onSuccess(null)
+        assertEquals(testNotes[0], result)
+    }
+
+
 }
--- app/src/test/java/com/jdeveloperapps/kotlinproject/ui/NoteViewModuleTest.kt
+++ app/src/test/java/com/jdeveloperapps/kotlinproject/ui/NoteViewModuleTest.kt
@@ -3,13 +3,14 @@ package com.jdeveloperapps.kotlinproject.ui
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.jdeveloperapps.kotlinproject.data.NotesRepository
 import com.jdeveloperapps.kotlinproject.data.entity.Note
+import com.jdeveloperapps.kotlinproject.ui.note.NoteData
 import com.jdeveloperapps.kotlinproject.ui.note.NoteViewModel
-import io.mockk.clearAllMocks
-import io.mockk.every
-import io.mockk.mockk
+import io.mockk.*
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class NoteViewModuleTest {
 
@@ -17,7 +18,6 @@ class NoteViewModuleTest {
     val taskExecutorRule = InstantTaskExecutorRule()
 
     private val mockRepository = mockk<NotesRepository>()
-//    private val noteLiveData = MutableLiveData<NoteResult>()
 
     private val testNote = Note("1", "title1", "text1")
     private lateinit var viewModel: NoteViewModel
@@ -25,59 +25,27 @@ class NoteViewModuleTest {
     @Before
     fun setup() {
         clearAllMocks()
-        every { runBlocking { mockRepository.getNoteById(testNote.id) } } returns testNote
-//        every { mockRepository.deleteNote(testNote.id) } returns noteLiveData
+        coEvery { mockRepository.getNoteById(testNote.id) } returns testNote
+        coEvery { mockRepository.deleteNote(testNote.id) } just runs
         viewModel = NoteViewModel(mockRepository)
     }
 
-//    @Test
-//    fun `loadNote should return Note Data`() {
-//        var result: NoteData? = null
-//        val testData = NoteData(false, testNote)
-//        viewModel.getViewState() {
-//            result = it?.data
-//        }
-//        viewModel.loadNote(testNote.id)
-//        noteLiveData.value = NoteResult.Success(testNote)
-//        assertEquals(testData, result)
-//    }
+    @Test
+    fun `loadNote should return Note Data`() = runBlocking {
+        var result: NoteData? = null
+        val testData = NoteData(false, testNote)
+        viewModel.loadNote(testNote.id)
+        result = viewModel.getViewState().receive()
+        assertEquals(testData, result)
+    }
 
-//    @Test
-//    fun `loadNote should return error`() {
-//        var result: Throwable? = null
-//        val testData = Throwable("error")
-//        viewModel.getViewState().observeForever {
-//            result = it?.error
-//        }
-//        viewModel.loadNote(testNote.id)
-//        noteLiveData.value = NoteResult.Error(error = testData)
-//        assertEquals(testData, result)
-//    }
-//
-//    @Test
-//    fun `deleteNote should return Note Data with isDeleted`() {
-//        var result: NoteViewState.Data? = null
-//        val testData = NoteViewState.Data(true, null)
-//        viewModel.getViewState().observeForever {
-//            result = it?.data
-//        }
-//        viewModel.save(testNote)
-//        viewModel.deleteNote()
-//        noteLiveData.value = NoteResult.Success(null)
-//        assertEquals(testData, result)
-//    }
-//
-//    @Test
-//    fun `deleteNote should return error`() {
-//        var result: Throwable? = null
-//        val testData = Throwable("error")
-//        viewModel.getViewState().observeForever {
-//            result = it?.error
-//        }
-//        viewModel.save(testNote)
-//        viewModel.deleteNote()
-//        noteLiveData.value = NoteResult.Error(error = testData)
-//        assertEquals(testData, result)
-//    }
+    @Test
+    fun `loadNote should return Note Data with isDeleted`() = runBlocking {
+        var result: NoteData? = null
+        val testData = NoteData(true, null)
+        viewModel.deleteNote()
+        result = viewModel.getViewState().receive()
+        assertEquals(testData, result)
+    }
 
 }
